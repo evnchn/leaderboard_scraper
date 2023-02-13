c27ae99d15f7f87d5e5bb0b06b35b7e1d47c9f80
Admin <admin@Admins-MacBook-Air.local>
2023-02-13 23:12:43 +0800

    sys.argv start with 1 for arguments

--- www/run_server.py
+++ www/run_server.py
@@ -5,7 +5,7 @@ import sys
 httpd = HTTPServer(('localhost', 6387), BaseHTTPRequestHandler)
 
 httpd.socket = ssl.wrap_socket (httpd.socket, 
-        keyfile=sys.argv[0], 
-        certfile=sys.argv[1], server_side=True)
+        certfile=sys.argv[1], 
+        keyfile=sys.argv[2], server_side=True)
 
 httpd.serve_forever()
